84d5c9cb54fc2cf4bf63416f1a2eb86f0c57e7b5
Matei-Albu <albumat0529@gmail.com>
2026-03-06 14:57:04 -0500

    update endpoint connection with ORM

--- app/main.py
+++ app/main.py
@@ -98,14 +98,21 @@ async def get_post(id: int, db: Session = Depends(get_db)):
     return {"post_details": post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post is None:
+async def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    deleted_post = db.query(models.Post).filter(models.Post.id == id)
+
+
+    if deleted_post.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id : {id} does not exist")
 
+    deleted_post.delete(synchronize_session=False)
+    db.commit()
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @app.put("/posts/{id}")
 async def update_post(id:int, post: Post):
