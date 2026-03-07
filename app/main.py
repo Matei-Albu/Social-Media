ace79df529ed59a3e066742afee773bfc94cdca3
Matei-Albu <albumat0529@gmail.com>
2026-03-07 15:30:54 -0500

    create schemas file

--- app/main.py
+++ app/main.py
@@ -8,18 +8,13 @@ from psycopg2.extras import RealDictCursor
 import time
 import os
 from dotenv import load_dotenv
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 load_dotenv()
 
@@ -59,12 +54,6 @@ def find_index_post(id):
 async def root():
     return "root"
 
-@app.get("/sqlalchemy")
-async def test_post(db: Session = Depends(get_db)):
-
-    posts = db.query(models.Post).all()
-    return{"data": posts}
-
 @app.get("/posts")
 async def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts """)
@@ -74,7 +63,7 @@ async def get_posts(db: Session = Depends(get_db)):
     return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_posts(post: Post, db: Session = Depends(get_db)):
+async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title,content, published) VALUES(%s ,%s ,%s ) RETURNING * """, (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
@@ -115,7 +104,7 @@ async def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-async def update_post(id:int, updated_post: Post, db: Session = Depends(get_db)):
+async def update_post(id:int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """ , (post.title, post.content, post.published, id))
     # updated_post = cursor.fetchone()
     # conn.commit()
