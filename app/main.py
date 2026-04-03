2477cfbe9c0cf15808fb9d7911c89dad3f267daf
Matei-Albu <albumat0529@gmail.com>
2026-04-03 18:39:07 -0400

    add JWT token creation

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -53,6 +53,7 @@ def find_index_post(id):
         
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 async def root():
